5dc52e25436d3d51e24610234b74b8e593f31427
steveoni <steohenoni2@gmail.com>
2020-12-03 17:33:04 +0100

    [data][s]: test dataset

---
+++ data.js
@@ -0,0 +1,10 @@
+import * as paraguay from './fixtures/paraguay.json'
+import * as uruguay from './fixtures/uruguay.json'
+
+module.exports = {
+  data : ["test1", "test2", "test3"],
+  catalog: {
+    'paraguay': paraguay,
+    'uruguay': uruguay
+  }
+}
