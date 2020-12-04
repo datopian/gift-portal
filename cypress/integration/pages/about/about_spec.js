8c2ba000818efcff47c223f7c54cac081b3d278c
steveoni <steohenoni2@gmail.com>
2020-12-04 16:13:29 +0100

    [test][s]: about page test

---
+++ cypress/integration/pages/about/about_spec.js
@@ -0,0 +1,13 @@
+describe('Dataset page test', function() {
+  
+  it('contains about property 1', function (){
+    cy.visit('/about/gift2')
+    cy.contains("Title: Gift v2.0 dataset");
+    cy.contains("author: Nicky Vibes");
+  });
+  it('contains about property 2', function (){
+    cy.visit('/about/gift1')
+    cy.contains("Title: Gift dataset");
+    cy.contains("date: Dec 3 2020");
+  });
+})
