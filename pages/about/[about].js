3450668bfe7f4ee3773af3b6f10c250e3257fb25
steveoni <steohenoni2@gmail.com>
2020-12-04 16:18:23 +0100

    [about][s]: add about page

---
+++ pages/about/[about].js
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router'
+import { Remarkable }  from 'remarkable';
+import { getAbout } from '../../lib/about-utils';
+import markdownStyles from '../../styles/Markdown.module.css';
+
+function About({content, data, isEmpty}){
+
+  let md = new Remarkable()
+
+  if(isEmpty){
+    return <div>404</div>
+  }else{
+    return (
+      <article className="max-w-2xl mx-auto mt-20">
+        <div>
+          <div >Title:  <span className='text-black-1000 font-bold p-5 m-2'>{data.title}</span></div>
+          <div >date:  <span className='text-black-1000 font-thin p-5 m-2'>{data.date}</span></div>
+          <div >author: <span className='text-black-1000 font-thin p-5 m-2'>{data.author}</span></div>
+        </div>
+        
+        <div className='mt-10'>
+          <div 
+           className={markdownStyles['markdown']}
+           dangerouslySetInnerHTML={{ __html: md.render(content) }}/>
+        </div>
+      </article>
+      
+    );
+  }
+  
+}
+
+export async function getServerSideProps(context) {
+  const aboutProps = getAbout(context.query.about)
+
+  return {
+    props : {content: aboutProps.content,
+      data: aboutProps.data,
+      isEmpty: aboutProps.isEmpty
+    }
+  }
+}
+
+export default About
