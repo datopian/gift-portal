0e8f38098b0be02eaabb61e914dd307f418846ab
steveoni <steohenoni2@gmail.com>
2020-12-04 13:15:53 +0100

    [css][s]: include  tailwind css to pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 import * as Data  from '../data';
 
 export default function Home() {
   
   return (
-    <div className={styles.container}>
-      <h1>GIFT Data Portal</h1>
+    <div >
+      <h1 className='text-blue-600'>GIFT Data Portal</h1>
       <p>{Data.data[0]}</p>
     </div>
   )
