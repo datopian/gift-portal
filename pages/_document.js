ee112adf3699d3622d95f07c0ad08748550da667
steveoni <steohenoni2@gmail.com>
2020-12-05 02:49:53 +0100

    [_document.js][s]: add google fonts for lato and karla

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+         <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+        </body>
+        <NextScript />
+      </Html>
+    );
+  }
+}
