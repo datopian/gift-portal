a75818c4544b9bd91e758799f686e9e87b6cc638
steveoni <steohenoni2@gmail.com>
2020-12-05 23:07:03 +0100

    [test][s]: test footer component

--- pages/test.js
+++ pages/test.js
@@ -1,19 +1,12 @@
 import * as Data  from '../data';
-import Card from '../components/Card';
+// import Card from '../components/Card';
+import Footer from '../components/Footer';
 
 export default function Home() {
   
-  let props = {
-    logo: '/argentina.svg',
-    name: 'Daily Treasury Statement (DTS)',
-    tags: ['Budget', 'FEDERAL','SPENDING','TAG4'],
-    description: 'CONTENT HERE',
-    created: '10/03/2019-11/26/2020',
-    updated: '11/30/2020',
-  }
   return (
     
-      <Card props={props}/>
+      <Footer />
 
   )
 }
