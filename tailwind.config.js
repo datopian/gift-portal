19dcb0b634a390500addb87f049f36a540559237
steveoni <steohenoni2@gmail.com>
2020-12-05 22:01:34 +0100

    [config][s]: configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,18 @@ module.exports = {
        'portal5': '#7C6990',
        'portal6': '#FFFFFF',
       }),
+    textColor: theme => theme('colors'),
+     textColor: {
+       'portal1': '#E81F76',
+     },
+    width :{
+      '379.25px': '379.25px',
+      '291.26px': '291.26px'
+    },
+    height : {
+      '607.81px' : '607.81px',
+      '97.09px': '97.09px'
+    }
   },
   variants: {
     extend: {},
