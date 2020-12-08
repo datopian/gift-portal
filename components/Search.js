2b3c8fc97f65f8a868e91679792473f13a483791
steveoni <steohenoni2@gmail.com>
2020-12-08 14:30:03 +0100

    [Search.js][s]: add hover effect

--- components/Search.js
+++ components/Search.js
@@ -1,7 +1,7 @@
 export default function Search(){
  
   return (
-    <div className='h-72px w-545px border-2 flex justify-between p-2 rounded-md font-karla'>
+    <div className='h-72px w-545px border-2 flex justify-between p-2 rounded-md font-karla hover:border-portal1'>
       <input type='text' placeholder='Search for dataset by keyword..' size='50' className='focus:outline-none'/>
       <button className='focus:outline-none'><img src="/search2.svg" /></button>
     </div>
