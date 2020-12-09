65254608f27eab7bb11ce8aefac5ba7b8e16e865
Rising Odegua <risingodegua@gmail.com>
2020-12-09 18:03:15 +0100

    [load data][m]: Add functionalilty to read data into catalog

---
+++ db/db.js
@@ -0,0 +1,35 @@
+import { Dataset } from "frictionless.js";
+import fs from "fs";
+import { join } from "path";
+
+export const getCatalog = async (directories) => {
+  let catalogs = [];
+  for (let i = 0; i < directories.length; i++) {
+    let path = join(process.cwd(), "fixtures", directories[i]);
+    let dataset = await Dataset.load(path);
+    let catalog = {}
+    catalog[dataset._descriptor.title] = dataset._descriptor;
+    catalogs.push(catalog);
+
+    if (i == directories.length - 1) {
+      return catalogs;
+    }
+  }
+};
+
+export const getDirectories = () => {
+  return new Promise((resolve, reject) => {
+    const directoryPath = join(process.cwd(), "fixtures/");
+    const data_folder_names = [];
+
+    fs.readdir(directoryPath, function (err, data_folders) {
+      if (err) {
+        reject(err);
+      }
+      data_folders.forEach(function (data_folder) {
+        data_folder_names.push(data_folder);
+      });
+      resolve(data_folders);
+    });
+  });
+};
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,40 @@
-import * as Data  from '../data';
-import Card from '../components/Card';
-import Search from '../components/Search';
+import Card from "../components/Card";
+import Search from "../components/Search";
+import { getCatalog, getDirectories } from "../db/db";
 
-export default function Home() {
-   const props = {
-    logo: '/argentina.svg',
-    name: 'Daily Treasury Statement (DTS)',
-    tags: ['Budget', 'FEDERAL','SPENDING','TAG4'],
-    description: 'CONTENT HERE',
-    created: '10/03/2019-11/26/2020',
-    updated: '11/30/2020',
-  }
-  let data = [];
-  for(let i=0; i < 8; i++){
-    data.push(props);
-  }
+export default function Home({ catalogs }) {
   return (
-        <div className='pl-40 pr-40 pt-10 pb-10'>
-          <h3 className='font-lato text-xl text-black'>DataSet</h3>
-          <div className='flex flex-row justify-between mt-10'>
-            <Search />
-            <div className='flex justify-between items-center mr-35 pr-9'>
-              <h3 className="mr-4">Sort by: </h3>
-              <select id="cars" className='border-2 focus:outline-none bg-white font-karla h-72px w-545px font-karla rounded-md'>
-                <option value="AZ">Alphabetical Ascending (A to Z)   </option>
-                <option value="ZA">Alphabetical Descending (Z to A)  </option>
-              </select>
-            </div>
-          </div>
-          <div className='mt-10'>
-            <div className='mb-10'>showing 6 of 6 dataset</div>
-            <div className='grid grid-cols-3 gap-x-40 gap-y-10'>
-              {data.map((value,i) => {
-                return <Card props={value} key={i}/>
-              })}
-            </div>
-          </div>
+    <div className="pl-40 pr-40 pt-10 pb-10">
+      <h3 className="font-lato text-xl text-black">DataSet</h3>
+      <div className="flex flex-row justify-between mt-10">
+        <Search />
+        <div className="flex justify-between items-center mr-35 pr-9">
+          <h3 className="mr-4">Sort by: </h3>
+          <select
+            id="cars"
+            className="border-2 focus:outline-none bg-white font-karla h-72px w-545px font-karla rounded-md"
+          >
+            <option value="AZ">Alphabetical Ascending (A to Z) </option>
+            <option value="ZA">Alphabetical Descending (Z to A) </option>
+          </select>
         </div>
-  )
+      </div>
+      <div className="mt-10">
+        <div className="mb-10">showing 6 of 6 dataset</div>
+        <div className="grid grid-cols-3 gap-x-40 gap-y-10">
+          {JSON.parse(catalogs).map((value, i) => {
+            return <Card props={JSON.stringify(value)} key={i} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  let data_directories = await getDirectories();
+  let catalogs = await getCatalog(data_directories);
+  return {
+    props: { catalogs: JSON.stringify(catalogs) },
+  };
 }
