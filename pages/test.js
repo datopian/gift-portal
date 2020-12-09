258ec496f166a8ed63c8ff2e95223b9e22de98c6
steveoni <steohenoni2@gmail.com>
2020-12-09 13:34:41 +0100

    [test.js][s]: a demo use case of the search event with fuse.io

--- pages/test.js
+++ pages/test.js
@@ -1,8 +1,8 @@
 import * as Data  from '../data';
 import Card from '../components/Card';
-import Footer from '../components/Footer';
 import Search from '../components/Search';
-import Navbar from '../components/Navbar';
+import Fuse from 'fuse.js'
+import { useState } from 'react';
 
 export default function Home() {
    const props = {
@@ -14,16 +14,36 @@ export default function Home() {
     updated: '11/30/2020',
   }
   let data = [];
+
   for(let i=0; i < 8; i++){
-    data.push(props);
+    if(i % 2===0){
+      let g = {...props, tags: props.tags.slice(0,2), name:'Paruguay Dataset'}
+      data.push(g);
+    }else{
+      data.push(props);
+    }
+    
   }
-  return (
-      <div>
-        <Navbar />
+  const [state, setstate] = useState(data);
+
+  const fuse = new Fuse(data, {
+    keys: ['name', 'tags'] 
+  });
+  
+  const handlSearch = function(keyword){
+      let data = fuse.search(keyword);
+      data = data.map((value)=>{
+          let {item} = value;
+          return item;
+      })
+      setstate(data);
+  }
+
+  return (  
         <div className='pl-40 pr-40 pt-10 pb-10'>
           <h3 className='font-lato text-xl text-black'>DataSet</h3>
           <div className='flex flex-row justify-between mt-10'>
-            <Search />
+            <Search  submbitEvent={handlSearch}/>
             <div className='flex justify-between items-center mr-35 pr-9'>
               <h3 className="mr-4">Sort by: </h3>
               <select id="cars" className='border-2 focus:outline-none bg-white font-karla h-72px w-545px font-karla rounded-md'>
@@ -35,15 +55,12 @@ export default function Home() {
           <div className='mt-10'>
             <div className='mb-10'>showing 6 of 6 dataset</div>
             <div className='grid grid-cols-3 gap-x-40 gap-y-10'>
-              {data.map((value,i) => {
+              {state.map((value,i) => {
                 return <Card props={value} key={i}/>
               })}
             </div>
           </div>
-        </div>
-        <Footer />
-      </div>
-      
+        </div>      
 
   )
 }
