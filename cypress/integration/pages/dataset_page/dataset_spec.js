38e3b78f96aab5fbcb6269938173fda2efd0db38
steveoni <steohenoni2@gmail.com>
2020-12-10 17:49:43 +0100

    [cypress][s]" update cypress test

--- cypress/integration/pages/dataset_page/dataset_spec.js
+++ cypress/integration/pages/dataset_page/dataset_spec.js
@@ -1,9 +1,9 @@
 describe('Dataset page test', function() {
   beforeEach(() => {
-    cy.visit('/dataset/paraguay');
+    cy.visit('/dataset/AT33.Tirol%202007-2013%20ERDF');
   });
 
   it('contains dataset name', function (){
-    cy.contains("Presupuesto de Gastos de la Nación - Paraguay");
+    cy.contains("AT33.Tirol 2007-2013 ERDF");
   })
 })
