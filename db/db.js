a9d7a8c68eed0bc4a3ead0ecb99000faede0d48b
Rising Odegua <risingodegua@gmail.com>
2020-12-10 16:41:45 +0100

    [data][m]: Update data retrieving function

--- db/db.js
+++ db/db.js
@@ -1,14 +1,30 @@
 import { Dataset } from "frictionless.js";
 import fs from "fs";
 import { join } from "path";
+import toArray from "stream-to-array";
 
+/**
+ * Returns a list of data packages descriptors found in db directory
+ * Descriptors are retrieved using frictionless.js.
+ * @param {Array} directories an array of directories containing datapacka.json's
+ * @returns {Array} List of data descriptors
+ */
 export const getCatalog = async (directories) => {
   let catalogs = [];
   for (let i = 0; i < directories.length; i++) {
     let path = join(process.cwd(), "fixtures", directories[i]);
     let dataset = await Dataset.load(path);
-    let catalog = {}
+    // let sample = dataset.resources[0].row({size: 2000})
+    let file = await dataset.resources[0];
+    await file.addSchema();
+    let schema = file.descriptor.schema;
+    let sample_stream = await file.rows({ size: file.size });
+    let sample = await toArray(sample_stream);
+    // console.log(sample);
+    let catalog = {};
     catalog[dataset._descriptor.title] = dataset._descriptor;
+    catalog["schema"] = schema;
+    catalog["sample"] = sample;
     catalogs.push(catalog);
 
     if (i == directories.length - 1) {
@@ -17,6 +33,9 @@ export const getCatalog = async (directories) => {
   }
 };
 
+/**
+ * Returns a list of directory names found in fixtures folder
+ */
 export const getDirectories = () => {
   return new Promise((resolve, reject) => {
     const directoryPath = join(process.cwd(), "fixtures/");
