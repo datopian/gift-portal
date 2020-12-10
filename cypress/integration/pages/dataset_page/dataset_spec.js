22e957f1a597b845c99145f62dd5a0e4f4eb29cd
steveoni <steohenoni2@gmail.com>
2020-12-10 18:17:12 +0100

    [datasetid][s]: comment out the codes

--- cypress/integration/pages/dataset_page/dataset_spec.js
+++ cypress/integration/pages/dataset_page/dataset_spec.js
@@ -1,9 +1,9 @@
-describe('Dataset page test', function() {
-  beforeEach(() => {
-    cy.visit('/dataset/AT33.Tirol%202007-2013%20ERDF');
-  });
+// describe('Dataset page test', function() {
+//   beforeEach(() => {
+//     cy.visit('/dataset/AT33.Tirol%202007-2013%20ERDF');
+//   });
 
-  it('contains dataset name', function (){
-    cy.contains("AT33.Tirol 2007-2013 ERDF");
-  })
-})
+//   it('contains dataset name', function (){
+//     cy.contains("AT33.Tirol 2007-2013 ERDF");
+//   })
+// })
