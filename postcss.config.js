0e4b32baa609183f1932b74309b345f3b35a204b
steveoni <steohenoni2@gmail.com>
2020-12-14 17:38:59 +0100

    [css][s]: update css configuration to work at production

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const purgecss = [
     // Specify the paths to all of the template files
     content: [
       './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
+      './components/*.{js,jsx,ts,tsx}',
     ],
     // This is the function used to extract class names from the templates
     defaultExtractor: (content) => {
@@ -17,9 +17,5 @@ const purgecss = [
   },
 ];
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    process.env.NODE_ENV === 'production' ? purgecss : undefined,
-    'postcss-preset-env',
-  ],
+  plugins: ['tailwindcss', 'autoprefixer']
 };
