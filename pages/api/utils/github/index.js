e69bf188a819b4479379959b713ede1b398b90e4
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-21 18:21:42 -0300

    [github] [s]: method to list user organizations

--- pages/api/utils/github/index.js
+++ pages/api/utils/github/index.js
@@ -18,3 +18,28 @@ async function githubGraphqlAPI(token, query) {
   return response.data.data
 }
 
+async function fetchOrganizationList(token, data = [], pageCursor) {
+  const query = `query {
+    viewer  {
+      organizations(first: 100 ${pageCursor ? ',after: "' + pageCursor + '"' : ''}) {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          login
+        }
+      }
+    }
+  }`
+
+  const response = await githubGraphqlAPI(token, query)
+
+  data.push(...response.viewer.organizations.nodes.map(item => item.login))
+
+  if (response.viewer.organizations.pageInfo.hasNextPage) {
+    return fetchOrganizationList(token, data, response.viewer.organizations.pageInfo.endCursor)
+  }
+
+  return data
+}
