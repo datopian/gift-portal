c1e6e365cfe8b19de465784b5a98694c5c85cf20
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-24 08:05:14 -0300

    [refactor] [xl]: rename files

--- __tests__/lib/github.spec.js
+++ __tests__/lib/github.spec.js
@@ -1,4 +1,4 @@
-import * as authorization from '../../lib/authorization'
+import * as github from '../../lib/github'
 import axios from 'axios'
 import moxios from 'moxios'
 
@@ -6,7 +6,7 @@ describe('Github Permissions tests', () => {
 
   const collaboratorsList = [
     {
-      "login": "octocat",
+      "login": "test-user",
       "permissions": {
         "pull": true,
         "push": true,
@@ -16,13 +16,23 @@ describe('Github Permissions tests', () => {
   ]
 
   const datasetScope = {
-    organization: 'github',
+    organization: 'datopian',
     dataset: 'repotest',
-    editors: ['octocat'],
-    readers: ['octocat'],
+    editors: ['test-user'],
+    readers: ['test-user'],
     admin: []
   }
 
+  const token = {
+    success: true,
+    result: {
+      requested_scopes: ['obj:/datopian/repotest/*:write'],
+      granted_scopes: ['obj:/datopian/repotest/*:write'],
+      token: 'token',
+      user_id: ''
+
+    }
+  }
   beforeEach(() => {
     moxios.install(axios)
   })
@@ -37,7 +47,7 @@ describe('Github Permissions tests', () => {
       response: {}
     })
 
-    const githubApi = await authorization.fetchGithubApi('')
+    const githubApi = await github.fetchGithubApi('')
     expect(githubApi).toEqual({})
 
   })
@@ -45,29 +55,29 @@ describe('Github Permissions tests', () => {
   it('should get a list of collaborators from some repository', async () => {
 
 
-    moxios.stubRequest('https://api.github.com/repos/github/octocat/collaborators', {
+    moxios.stubRequest('https://api.github.com/repos/datopian/octocat/collaborators', {
       status: 200,
       response: collaboratorsList
     })
 
-    const collaborators = await authorization.getRepositoryCollaborators('github', 'octocat')
+    const collaborators = await github.getRepositoryCollaborators('repotest')
     expect(collaborators).toEqual(collaboratorsList)
   })
 
   it('should format the list of collaborators', () => {
 
     const defaultList = collaboratorsList
-    const parsedList = authorization.parserCollaboratorsList('github', 'repotest', defaultList)
+    const parsedList = github.parserCollaboratorsList('repotest', defaultList)
 
     expect(parsedList).toEqual(datasetScope)
   })
 
   it('should thow an error if user is not inside dataset scope', () => {
-    expect(() => authorization.isValidScope('github', 'repotest', 'tester')).toThrow('Invalid scope. Scope should be of form "github/repotest:read/write/admin')
+    expect(() => github.isValidScope('repotest', 'tester')).toThrow('Invalid scope. Scope should be of form "datopian/repotest:read/write/admin')
   })
 
-  it('should give and user authorization token', () => {
-
+  it('should return an user github token', () => {
+    
   })
 
   it('should get the organization, username and return a token', ()=>{
