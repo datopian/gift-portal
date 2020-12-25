302da0db29b928a33ea8d00133b10a2a9b48628f
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-25 17:20:47 -0300

    [script] [s] : basic script test

--- scripts/githubOrg.js
+++ scripts/githubOrg.js
@@ -1,2 +1,19 @@
-import Github from '../lib/Github'
 import fs from 'fs'
+import path from 'path'
+import Github from '../lib/Github'
+
+const github = new Github()
+
+async function createDataDirIfNotExists(){
+  const rootPath = path.resolve(process.cwd())
+  const isDirExists = await fs.existsSync(`${rootPath}/data`)
+  if(!isDirExists) await fs.mkdirSync(`${rootPath}/data`)
+}
+
+
+async function run(){
+  await createDataDirIfNotExists()
+}
+
+
+run()
