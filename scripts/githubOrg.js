387f6e4fe47c80dab14c39f8f8172e3e2d23477a
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-27 16:09:44 -0300

    [script] [l] : script workflow to get org datasets

--- scripts/githubOrg.js
+++ scripts/githubOrg.js
@@ -2,18 +2,45 @@ import fs from 'fs'
 import path from 'path'
 import Github from '../lib/Github'
 
+require('dotenv').config()
+
 const github = new Github()
 
+/**
+ * Create Data Directory if not exists
+ */
 async function createDataDirIfNotExists(){
   const rootPath = path.resolve(process.cwd())
   const isDirExists = await fs.existsSync(`${rootPath}/data`)
   if(!isDirExists) await fs.mkdirSync(`${rootPath}/data`)
 }
 
-
-async function run(){
-  await createDataDirIfNotExists()
+/**
+ * 
+ * @param {Object} data - Store data as JSON  
+ */
+async function createDataAsJson(data){
+  const rootPath = path.resolve(process.cwd())
+  fs.writeFileSync(`${rootPath}/data/datasets.json`, JSON.stringify(data, null, 2))
 }
 
+/**
+ * Get Organization list and scopes
+ */
+async function getOrganizationScopes(){
+  return github.getOrgRepos()
+    .then(data => data.repositories.map(item => item.name))
+    .then(data => data.map(item => github.getScopes(item)))
+    .then(data => Promise.all(data)) 
+}
+
+/**
+ * Script Workflow
+ */
+function run(){
+  createDataDirIfNotExists()
+  .then(getOrganizationScopes)
+  .then(createDataAsJson)
+}
 
 run()
