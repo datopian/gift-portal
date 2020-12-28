364ec1b0907a467394c092f50563c1cfcf65cc5b
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-28 12:36:20 -0300

    [refactor] [xs] : update file names

--- __tests__/lib/Permissions.js
+++ __tests__/lib/Permissions.js
@@ -1,9 +1,13 @@
-import * as auth from '../../lib/Authorize'
+import Permissions from '../../lib/Permissions'
+
+const permissions = new Permissions()
 
 describe('Authenticate Tests', ()=>{
      it('should thow an error if requested scope is invalid', () => {
-      expect(() => auth.isValidScope('repotest', 'tester', 'datopian'))
+      expect(() => permissions.isValidScope('repotest', 'tester', 'datopian'))
         .toThrow('Invalid scope. Scope should be of form "datopian/repotest:read/write/admin')
     })
+
+    
     
 })
