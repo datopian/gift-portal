f7421ac2708594ec21fe8c6b367d5061c80b7871
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-28 12:25:06 -0300

    [authorize] [s] : update file names

--- __tests__/lib/authorize.spec.js
+++ __tests__/lib/authorize.spec.js
@@ -1,8 +1,9 @@
-import * as auth from '../../lib/Authenticate'
+import * as auth from '../../lib/Authorize'
 
 describe('Authenticate Tests', ()=>{
      it('should thow an error if requested scope is invalid', () => {
       expect(() => auth.isValidScope('repotest', 'tester', 'datopian'))
         .toThrow('Invalid scope. Scope should be of form "datopian/repotest:read/write/admin')
     })
+    
 })
