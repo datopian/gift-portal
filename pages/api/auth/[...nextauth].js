af18245b416c59e867c2f79a770394fd1cd6c429
steveoni <steohenoni2@gmail.com>
2020-12-28 16:38:49 +0100

    [..nextauth.js][s]: set state to false

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,8 @@ const options = {
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      state: false,
     })
   ],
   pages : {
