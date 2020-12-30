0ed11ebb22f308b7448d62b41fb624eec3e4a8e1
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-30 19:54:02 -0300

    [lint] [m] : setup config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports={
-  "presets": ["next/babel"],
-  "plugins": ["@babel/plugin-proposal-do-expressions"]
+module.exports = {
+  presets: ['next/babel'],
+  plugins: ['@babel/plugin-proposal-do-expressions'],
 }
