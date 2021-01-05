65faea5896d76756a90aa0db7ae02c8ea5f12d49
steveoni <steohenoni2@gmail.com>
2021-01-05 19:14:48 +0100

    [next-config][s]: to prevent filesystem error

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config;
+  },
+};
