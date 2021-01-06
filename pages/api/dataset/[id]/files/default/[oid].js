b0b82afa4e2417adf42f6c0e6b813ba74b849ca5
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-01-06 10:03:22 -0300

    [api] [s] : add main api block

---
+++ pages/api/dataset/[id]/files/default/[oid].js
@@ -0,0 +1,11 @@
+export default async function handler(req,res){
+  try{ /**
+   * TODO 
+   * Strategy to get username to check access
+   */
+
+    
+  }catch(error){
+    res.status(400).send('Error on Retrieve Resource')
+  }
+}
