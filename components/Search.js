367072dc7407e253cbdf6407ec2f42a30219c225
Sebastien Lavoie <sebastien.lavoie@datopian.com>
2021-01-13 14:12:24 -0600

    [css,search][s]: Make Search section responsive on all devices

--- components/Search.js
+++ components/Search.js
@@ -5,8 +5,8 @@ import { useRef } from 'react'
 export default function Search({submbitEvent}){
   let inputref = useRef()
   return (
-    <div className='w-545px border-2 flex justify-between p-2 rounded-md font-karla hover:border-portal1 p-4'>
-      <input type='text' placeholder='Search for dataset by keyword...' size='50' className='focus:outline-none' ref={inputref}/>
+    <div className='border-2 flex justify-between p-2 rounded-md font-karla md:p-4 hover:border-portal1'>
+      <input type='text' placeholder='Search for dataset by keyword...' size='30' className='focus:outline-none' ref={inputref}/>
       <button className='focus:outline-none' onClick={()=> { submbitEvent(inputref.current.value)}}><img src="/search2.svg" /></button>
     </div>
   )
