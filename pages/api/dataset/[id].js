1dbab8f17602d615a4cf6f4a390fc5f43ebd7af2
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-01-17 13:46:58 -0300

    [api] [m]: route to update metadata

--- pages/api/dataset/[id].js
+++ pages/api/dataset/[id].js
@@ -6,22 +6,35 @@ const permissions = new Permissions()
 
 export default function handler(req,res){
 
+  const { id: objectId} = req.query
+  const { user } = req.body
+
+  if(!permissions.userHasPermission(
+    user.username, 
+    objectId, 
+    'write')) {
+    res.status(401).send('Unauthorized User')
+  }
+
   if(req.method === 'POST'){
     /**
      * TBD - Get User information
      */
-    const { id: objectId} = req.query
-    const { user } = req.body
-    if(!permissions.userHasPermission(
-      user.username, 
-      objectId, 
-      'write')) {
-      res.status(401).send('Unauthorized User')
-    }
     const { metadata: data, description }= req.body
 
     metadata
       .createMetadata(objectId, user, data, description)
+      .then(data => res.send(data))
+      .catch(error => res
+        .status(400)
+        .send(`Error on create metadata: ${error.message}`)
+      )
+  }
+  if(req.method === 'PUT'){
+    const { metadata: data, readMe}  = req.body
+
+    metadata
+      .updateMetadata(objectId, user, data, readMe)
       .then(data => res.send(data))
       .catch(error => res
         .status(400)
