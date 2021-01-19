33db57d6d9435e9610122a0a288f0aea1dfb4f92
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-01-19 11:18:53 -0300

    [admin] [s] : update giftless-server key from admin

--- pages/admin/publisher.js
+++ pages/admin/publisher.js
@@ -6,7 +6,7 @@ export default function Publisher() {
   const config = {
     datasetId: 'sample_1',
     api: 'http://127.0.0.1:5000',
-    lfs: 'http://localhost:9419',
+    lfs: process.env.GIFTLESS_SERVER,
     authToken: 'be270cae-1c77-4853-b8c1-30b6cf5e9228',
     organisationId: 'myorg',
     resourceId: ''
