d76c424620ff8ea6a7f8faea53d5e4983ddd44d0
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-01-20 20:17:59 -0300

    [metadata] [m] : comment permissions to test

--- pages/api/dataset/[id].js
+++ pages/api/dataset/[id].js
@@ -1,29 +1,31 @@
 import { getSession } from 'next-auth/client'
 import Metadata from '../../../lib/Metadata'
-import Permissions from '../../../lib/Permissions'
+// import Permissions from '../../../lib/Permissions'
 
 const metadata = new Metadata()
-const permissions = new Permissions()
+// const permissions = new Permissions()
 
 export default async function handler(req,res){
   try{
     const session = await getSession({ req })
     const { id: objectId} = req.query
     const { user } = session
-    const userToken = session.token.accessToken
+
+    const userToken = process.env.APP_GITHUB_KEY
   
-    if(!permissions.userHasPermission(
-      user.login, 
-      objectId, 
-      'write')) {
-      res.status(401).send('Unauthorized User')
-    }
+    // if(!permissions.userHasPermission(
+    //   user.login, 
+    //   objectId, 
+    //   'write')) {
+    //   res.status(401).send('Unauthorized User')
+    // }
   
+   
     if(req.method === 'POST'){
      
       const { metadata: data, description }= req.body
   
-      metadata
+      return metadata
         .createMetadata(objectId, user, data, description, userToken)
         .then(data => res.send(data))
         .catch(error => res
@@ -34,7 +36,7 @@ export default async function handler(req,res){
     if(req.method === 'PUT'){
       const { metadata: data, readMe}  = req.body
   
-      metadata
+      return metadata
         .updateMetadata(objectId, user, data, readMe, userToken)
         .then(data => res.send(data))
         .catch(error => res
@@ -47,6 +49,7 @@ export default async function handler(req,res){
     }
   }
   catch(error){
+    console.log(error)
     res.status(400).json({ error: 'Bad Request', message: error.message})
   }
 
