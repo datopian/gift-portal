4edc787491ee225c3d4f7a04f1da59223828101f
steveoni <steohenoni2@gmail.com>
2021-01-20 01:31:53 +0000

    [id.js][s]: include env variables

--- pages/admin/publisher/[id].js
+++ pages/admin/publisher/[id].js
@@ -5,18 +5,19 @@ export default function Publisher({ lfs, organisationId, datasetId }) {
   const config = {
     datasetId: datasetId,
     api: 'http://127.0.0.1:5000',
-    lfs: 'https://giftless-gift.herokuapp.com/',
+    lfs: lfs,
     authToken: 'be270cae-1c77-4853-b8c1-30b6cf5e9228',
-    organisationId: 'gift-data',
-    resourceId: ''
+    organisationId: organisationId,
+    resourceId: '',
   }
   // eslint-disable-next-line react/react-in-jsx-scope
   return <ResourceEditor config={config} resource="" />
 }
 
 Publisher.getInitialProps = async (ctx) => {
-  return { lfs: process.env.GIFTLESS_SERVE,
-    organisationId:  process.env.ORGANISATION_REPO,
-    datasetId: ctx.query.id
+  return {
+    lfs: process.env.GIFTLESS_SERVER,
+    organisationId: process.env.ORGANISATION_REPO,
+    datasetId: ctx.query.id,
   }
 }
