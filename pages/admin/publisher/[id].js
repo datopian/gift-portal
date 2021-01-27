9e9aa0ec7f78d068d24b0294009ee4f2a42e12fa
steveoni <steohenoni2@gmail.com>
2021-01-27 01:03:38 +0000

    [id][s]: refactor

--- pages/admin/publisher/[id].js
+++ pages/admin/publisher/[id].js
@@ -1,20 +1,31 @@
 import { ResourceEditor } from 'giftpub'
+import * as metastore from 'metastore-lib-js'
 
-export default function Publisher({ lfs, organisationId, datasetId }) {
+export default function Publisher({ lfsServerUrl, dataset }) {
 
   const config = {
-    datasetId: datasetId,
-    api: 'http://127.0.0.1:5000',
-    lfs: lfs,
-    authToken: 'be270cae-1c77-4853-b8c1-30b6cf5e9228',
-    organisationId: organisationId,
-    resourceId: '',
+    dataset: dataset,
+    lfs: lfsServerUrl,
+    authorizeApi: '/api/authorize/',
+    metastoreApi: '/api/dataset/'
   }
   // eslint-disable-next-line react/react-in-jsx-scope
-  return <ResourceEditor config={config} resource="" />
+  return <ResourceEditor config={config} resource='' />
 }
 
 Publisher.getInitialProps = async (ctx) => {
+  const config = {
+    token: 'personal_access_token',
+    defaultAuthor: { name: 'Stephen Oni', email: 'steohenoni2@gmail.com' },
+    org: process.env.ORGANISATION_REPO,
+    lfsServerUrl: process.env.GIFTLESS_SERVER
+  }
+  
+  const storage = metastore.createMetastore('github', config)
+
+  const objectInfo = await storage.fetch(ctx.query.id)
+  console.log(objectInfo)
+
   return {
     lfs: process.env.GIFTLESS_SERVER,
     organisationId: process.env.ORGANISATION_REPO,
