c00bcdff94c34383a4d8d72ed7fe4c59b2f9e06b
steveoni <steohenoni2@gmail.com>
2021-01-28 11:41:34 +0000

    [id][s]: change from api to call to funciton call

--- pages/admin/publisher/[id].js
+++ pages/admin/publisher/[id].js
@@ -1,20 +1,13 @@
+import React from 'react'
 import { DatasetEditor } from 'giftpub'
 import Error from 'next/error'
-import axios from 'axios'
-import React, { useEffect, useState, } from 'react'
+import { getSession } from 'next-auth/client'
+import Metadata from '../../../lib/Metadata'
 
-export default function Publisher({ lfsServerUrl, query, organizationId }) {
-  const [data, setData] = useState()
-
-  useEffect(()=>{
-    axios({
-      method: 'GET',
-      url: `/api/dataset/${query}`
-    }).then(res => setData(res.data))
-  }, [])
-
+export default function Publisher({ lfsServerUrl, dataset, organizationId }) {
+  
   const config = {
-    dataset: data,
+    dataset: dataset,
     lfsServerUrl: lfsServerUrl,
     authorizeApi: '/api/authorize/',
     metastoreApi: '/api/dataset/',
@@ -24,16 +17,26 @@ export default function Publisher({ lfsServerUrl, query, organizationId }) {
   // eslint-disable-next-line react/react-in-jsx-scope
   return (
     <>
-      { (data) && (<DatasetEditor config={config}/>) }
-      { (!data) && (<Error statusCode={404}/>)}
+      { (dataset) && (<DatasetEditor config={config}/>) }
+      { (!dataset) && (<Error statusCode={404}/>)}
     </>
   )
 }
 
-Publisher.getInitialProps = async (ctx) => {
+Publisher.getInitialProps = async (context) => {
+
+  const metadata = new Metadata()
+  const {req} = context
+  const session = await getSession({ req })
+  const { user } = session
+
+  const userToken = process.env.APP_GITHUB_KEY
+
+  const data = await metadata.fetchMetadata(context.query.id,user,userToken)
+
   return {
     lfsServerUrl: process.env.GIFTLESS_SERVER,
     organizationId: process.env.ORGANISATION_REPO,
-    query: ctx.query.id,
+    dataset: data
   }
 }
