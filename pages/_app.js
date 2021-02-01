2c792b323e53c788c821c7857a47c1445222bb4b
Rising Odegua <risingodegua@gmail.com>
2021-02-01 13:03:34 +0100

    [portal][l]: Update portal to initialize cache on start

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,34 @@
 /* eslint-disable react/react-in-jsx-scope */
-import '../styles/tailwind.css'
-import Layout from '../components/Layout'
-import { Provider } from 'next-auth/client'
-import '../styles/pub.css'
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apolloClient'
+import "../styles/tailwind.css";
+import Layout from "../components/Layout";
+import { Provider } from "next-auth/client";
+import "../styles/pub.css";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
+import { MetastoreApollo } from "../lib/MetastoreApollo";
 
-function MyApp({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps.initialApolloState)
+function MyApp({ Component, pageProps, metaStoreCache }) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
       <Provider session={pageProps.session}>
         <Layout>
-          <Component {...pageProps} />
+          <Component {...pageProps} metaStoreCache={metaStoreCache} />
         </Layout>
       </Provider>
     </ApolloProvider>
-  )
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  const metastore = new MetastoreApollo();
+  // Initialize Metastore Cache. This is made available in all pages
+  const metaStoreCache = await metastore.initCache();
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  return { pageProps, metaStoreCache };
+};
+
+export default MyApp;
