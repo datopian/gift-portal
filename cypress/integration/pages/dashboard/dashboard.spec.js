7c1ebd549faf3039d6bd1a716143a1263ec677ec
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-02-04 17:33:33 -0300

    [tests] [m] : add dashboard test

---
+++ cypress/integration/pages/dashboard/dashboard.spec.js
@@ -0,0 +1,18 @@
+describe('Dashboard Page', ()=> {
+  
+  it('should return a message if user not logged', ()=> {
+    cy.visit('/dashboard')
+    cy.contains('Login to see DashBoard')
+  })
+
+  it('should return a list of dashboards if the user is logged', ()=> {
+    cy.intercept({
+      method: 'GET',
+      url: '/api/auth/session'
+    }, {fixture: 'user.json'})
+    
+    cy.visit('/dashboard')
+
+    cy.get('#dashboard-list')
+  })
+})
