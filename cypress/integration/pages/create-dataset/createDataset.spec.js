54ef809cf4d8c573b59a5b4304db765cc2faaeda
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-02-05 17:38:17 -0300

    [tests] [xl] : add create dataset test workflow

---
+++ cypress/integration/pages/create-dataset/createDataset.spec.js
@@ -0,0 +1,44 @@
+import 'cypress-file-upload';
+
+describe('Create dataset workflow using a csv file', ()=> {
+  it('should create a dataset', ()=> {
+    cy.intercept({
+      method: 'GET',
+      url: '/api/auth/session'
+    }, {fixture: 'user.json'})
+    
+    cy.log('Acess the dashboard Page and see 2 items on the list')
+    cy.visit('/dashboard')
+
+    cy.log('Select the item with the label: Edit Fiscal Data Schema')
+    cy.get('#dashboard-list').find('.create-schema').first().click()
+
+    cy.log('Select Chose a file to upload')
+    cy.get('.upload-choose').find('.choose-btn').first().click()
+    
+    cy.log('Attach a csv file')
+    cy.get('.upload-area__drop__input')
+      .attachFile('neighbourhoods.csv', { subjectType: 'drag-n-drop'})
+
+    cy.log('Mock the response from Giftless server')
+    cy.intercept({
+      method: 'POST', 
+      // eslint-disable-next-line max-len
+      url: 'https://giftless-gift.herokuapp.com/gift-data/MDEwOlJlcG9zaXRvcnkzMzMwNzgyMDM=/objects/batch'
+    }, {
+      transfers:["multipart-basic","basic"],
+      operation:'upload',
+      objects:[
+        {
+          oid:"fb0710c22d13ec34fb33b7bddc294bdc9cb0a1e95a84df1f9f",
+          size:2020
+        }
+      ],
+      ref:{
+        name:"refs/heads/master"
+      }
+    })
+
+    
+  })
+})
