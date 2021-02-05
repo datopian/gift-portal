547835406f6513da230317b261499a94a5b82de4
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-02-05 10:27:20 -0300

    [tests] [s] : rename files

--- cypress/integration/pages/dataset_page/dataset.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Dataset page test', function() {
-  beforeEach(() => {
-    cy.visit('/dataset/presupuesto-de-uruguay')
-  })
-
-  it('contains dataset name', function (){
-    cy.contains('Presupuesto de Uruguay')
-  })
-})
