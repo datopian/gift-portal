1076ab40e7f67c3e6ce4e0c86ff2f96c73727e6f
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-02-12 16:45:04 -0300

    [fetch] [m] : add new param on apollo client

--- pages/api/dataset/[id].js
+++ pages/api/dataset/[id].js
@@ -15,24 +15,6 @@ export default async function handler(req, res) {
       res.status(401).send("Unauthorized User");
     }
 
-    // if(req.method === 'POST'){
-    //   const { metadata: data, description }= req.body
-
-    //   return metastore
-    //     .create(objectId,
-    //       user,
-    //       data,
-    //       description,
-    //       user.token.accessToken
-    //     )
-    //     .then(data => res.send(data))
-    //     .catch(error =>
-    //       res
-    //         .status(400)
-    //         .send(`Error on create metadata: ${error.message}`)
-    //     )
-    // }
-
     if (req.method === "POST") {
       const { metadata } = req.body;
       return metastore
