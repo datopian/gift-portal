f6c4ea2fc1fc8a9986533de9a9de2c73ad34523b
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-02-12 17:39:26 -0300

    [permissions] [l] : update permissions using apollo when download resource

--- pages/api/dataset/[id]/files/default/[...oid].js
+++ pages/api/dataset/[id]/files/default/[...oid].js
@@ -1,19 +1,25 @@
 import { decrypt } from '../../../../../../lib/jwt'
 import Download from '../../../../../../lib/Download'
+import Permissions from '../../../../../../lib/Permissions'
+import { initializeApollo } from '../../../../../../lib/apolloClient'
+import { PERMISSIONS } from '../../../../../../lib/queries'
 import axios from 'axios'
 
 
 const download = new Download()
 
 export default async function handler(req,res){
+  const apolloClient = initializeApollo();
+  await apolloClient.query({query: PERMISSIONS})
+  const permissions = new Permissions(apolloClient.cache.extract());
   try{ 
     const { userInfo } = req.cookies
     const user = decrypt(userInfo) || { login: 'PUBLIC'}
     
     const { id, oid } = req.query
     
-    if(!await download.checkDatasetPermission(id, user.login)){
-      return res.status(401).send('Insufficient Permissions')
+    if (!await permissions.userHasPermission(user.login, id, "read")) {
+      res.status(401).send("Unauthorized User");
     }
 
     const resourceUrl = await download.getUrl(id, oid, user.login)
