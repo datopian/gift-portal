9b372cfc4c28e0a07c3bcfb3af7d9a404082e4f4
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-02-14 21:19:27 -0300

    [tests] [s] : fix dataset test

--- cypress/integration/pages/dataset_page/dataset_spec.js
+++ cypress/integration/pages/dataset_page/dataset_spec.js
@@ -1,9 +1,9 @@
 describe('Dataset page test', function() {
   beforeEach(() => {
-    cy.visit('/dataset/presupuesto-mexico-2008-2019')
+    cy.visit('/dataset/presupuesto-de-uruguay')
   })
 
   it('contains dataset name', function (){
-    cy.contains('Presupuesto México 2008-2019')
+    cy.contains('Presupuesto de Uruguay')
   })
 })
