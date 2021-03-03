dbf5988f1cd679d4d71fd4ba6b56fe3a9f5ea52f
Sebastien Lavoie <sebastien.lavoie@datopian.com>
2021-03-03 12:04:41 -0600

    [dashboard,styles][s]: Restyle and center table

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -20,67 +20,67 @@ export default function Dashboard({name, datasets}) {
   }
 
   return (
-    <div id="dashboard-list">
-      <div className="grid grid-rows-1 grid-flow-col place-content-center">
+    <div id='dashboard-list'>
+      <div className='grid grid-rows-1 grid-flow-col place-content-center'>
         <div>
-          <h1 className="dashboard-text-h1">Hi {name}.</h1>
+          <h1 className='dashboard-text-h1'>Hi {name}.</h1>
         </div>
       </div>
-      <div className="justify-center">
-        <h1 className="text-lg leading-6 font-medium text-gray-900 text-center">
+      <div className='justify-center'>
+        <h1 className='text-lg leading-6 font-medium text-gray-900 text-center'>
           {datasets.yourDatasets}
         </h1>
-        <div className="flex flex-col">
-          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
+        <div className='flex flex-col items-center justify-center'>
+          <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mb-4'>
+            <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+              <div className='overflow-hidden sm:rounded-lg'>
+                <table className='min-w-full divide-y'>
+                  <thead>
                     <tr>
                       <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        scope='col'
+                        className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
                       ></th>
-                      <th scope="col" className="relative px-6 py-3">
-                        <span className="sr-only">Edit</span>
+                      <th scope='col' className='relative px-6 py-3'>
+                        <span className='sr-only'>Edit</span>
                       </th>
                     </tr>
                   </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
+                  <tbody className='bg-white divide-y'>
                     {datasets.map((repo, i) => {
                       return (
-                        <tr key={`${i}-index`} id="dataset-list">
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="flex items-center">
-                              <div className="ml-4">
-                                <div className="text-sm font-medium text-gray-900">
+                        <tr key={`${i}-index`} id='dataset-list'>
+                          <td className='px-6 py-4 whitespace-nowrap'>
+                            <div className='flex items-center'>
+                              <div className='ml-12'>
+                                <div className='text-sm font-medium text-gray-900'>
                                   {repo.name}
                                 </div>
                               </div>
                             </div>
                           </td>
 
-                          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                          <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
                             {repoHasResource(repo) ? (
-                              <a 
+                              <a
                                 href={`/admin/publisher/${repo.name}`}
-                                className="edit-schema text-indigo-600 hover:text-indigo-900"
+                                className='edit-schema dashboardLink'
                               >
                                 Edit fiscal data schema
                               </a>
                             ) : (
-                              <a 
+                              <a
                                 href={`/admin/publisher/${repo.name}`}
-                                className="create-schema text-indigo-600 hover:text-indigo-900"
+                                className='create-schema dashboardLink'
                               >
-                                  Create a fiscal data schema
+                                Create a fiscal data schema
                               </a>
                             )}
                           </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                          <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium pr-16'>
                             <a
                               href={`/admin/publisher/edit-metadata/${repo.name}`}
-                              className="text-indigo-600 hover:text-indigo-900"
+                              className='dashboardLink'
                             >
                               Edit metadata
                             </a>
