a173cf61ca0b1b8da3ddf3d78ec5fb2e42c599e5
steveoni <steohenoni2@gmail.com>
2021-10-07 14:16:05 +0000

    [org][s]: add deleting of files after downloading

--- pages/api/dataset/all/[datasetid]/org/a/[org].js
+++ pages/api/dataset/all/[datasetid]/org/a/[org].js
@@ -61,6 +61,7 @@ export default async function handler(req, res) {
     await bucket.combine(newFileStorage, mergeFile)
     
     await download(mergeFile, res)
+    mergeFile.delete()
     // download(mergeFile, res).then(res => {
     //   mergeFile.delete()
     // })
