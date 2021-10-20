7a630427a79fadb209371505561104ebfc4bae26
steveoni <steohenoni2@gmail.com>
2021-10-20 14:46:30 +0100

    [_document.js][m]: add google analaytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,22 @@ export default class CustomDocument extends Document {
     return (
       <Html>
         <Head>
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet"></link>
 
